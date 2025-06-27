9203ba96ef8f6d8ab95d0083f537dfbd5390f9b9
Ethan-georlette <Ethan.georlette1@gmail.com>
2025-06-27 13:54:33 +0300

    second test prep

--- testapp.c
+++ testapp.c
@@ -1,9 +1,25 @@
 #include <stdio.h>
+/*
+int find_c(char c, const char *str) {
+    int count = 0;
+    while (*str) {
+        if (*str == c) count++;
+        str++;
+    }
+    return count;
+}
 
 int main(){
-	int x=5;
-	char exp= (char)x;
-	char hasa=x;
-	printf("explict=%c hasa=%c \n",exp,hasa);
-	return 1;
+	char *str = "fighjudredifind";
+	char c= 'd';
+	int count = find_c(c, str);
+	printf("Character '%c' found %d times in the string \"%s\".\n",c, count, str);
+	return 0;
+
+}
+	*/
+
+void main(void){
+	char vec[]= "abcdefghij";
+	putchar(vec[vec[1]-vec[0]]+=1);
 }
